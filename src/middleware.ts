b442c506c7b64c7d8de2c737c4e91bffba27d0a6
varunraj <vkvarunraj@gmail.com>
2026-02-28 22:28:43 +0530

    update website

--- src/middleware.ts
+++ src/middleware.ts
@@ -72,6 +72,6 @@ export const config = {
          * - favicon.ico (favicon file)
          * Feel free to modify this pattern to include more paths.
          */
-        '/((?!_next/static|_next/image|favicon.ico|manifest.json|apple-touch-icon.png|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
 }
